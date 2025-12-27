4c9f828b2289496cb1ff23ec9bf66fa7af8083e2
minhquan00 <138519309+minhquan00@users.noreply.github.com>
2025-12-27 17:21:32 +0700

    Update capcha.js

--- capcha.js
+++ capcha.js
@@ -443,7 +443,7 @@ async function startthread(targetURL, browserProxy, task, done, retries = 0) {
             
             try {
                 spawn("node", [
-                    "tes.js",
+                    "flooder.js",
                     targetURL,
                     duration, 
                     thread,
@@ -516,5 +516,6 @@ main().catch(err => {
     colored(colors.COLOR_RED, "[ERROR] Main function error: " + err.message);
     process.exit(1)
 });
+
 
 
